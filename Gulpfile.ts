63e251c0a05fc6249c17e4820cec826125bc8963
Stefan Grönke <stefan@gronke.net>
2017-10-18 23:56:52 +0200

    setup build process and draft reporter

---
+++ Gulpfile.ts
@@ -0,0 +1,49 @@
+/// <reference types="node" />
+
+const map = require('map-stream');
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
+const plugins = gulpLoadPlugins();
+
+import { GitHub, Statuses, StatusOptions } from './index';
+const githubReporter = new GitHub(plugins.util.env.GITHUB_TOKEN);
+const commit = githubReporter.commit(plugins.util.env.COMMIT);
+
+gulp.task('compile:typescript', () => {
+  return gulp.src('index.ts')
+  .pipe(plugins.typescript.createProject('tsconfig.json')())
+  .pipe(gulp.dest('.'));
+});
+
+gulp.task('tslint', () => {
+
+  let errorCount = 0;
+  const status = commit.getStatus('tslint');
+  return status.report(Statuses.pending)
+    .then(() => {
+      return gulp.src([
+        './index.ts'
+      ])
+      .pipe(plugins.debug())
+      .pipe(plugins.tslint({
+        configuration: 'tslint.json',
+        formatter: 'prose'
+      }))
+      .pipe(map((file: { tslint: { errorCount: number }}, done: (err: Error | null, file: {}) => void) => {
+        errorCount += file.tslint.errorCount;
+        done(null, file);
+      }))
+      .pipe(plugins.tslint.report({
+        emitError: false
+      }));
+    })
+    .then(() => {
+      const hasErrors = (errorCount > 0);
+      const state = hasErrors ? Statuses.failure : Statuses.success;
+      const description = hasErrors ? `failed with ${errorCount} errors` : '';
+      return status.report(state, description);
+    });
+
+});
+
+gulp.task('build', ['compile:typescript']);
---
+++ README.md
@@ -0,0 +1,4 @@
+Commit Status Reporter
+======================
+
+This utility written in TypeScript uses the [GitHub Statuses API](https://developer.github.com/v3/repos/statuses/) to report build stauses that are visible in GitHub Pull-Requests.
