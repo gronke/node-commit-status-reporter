a5afc42692d8f16316e7c6720002277f09f1fdec
Stefan Grönke <stefan@gronke.net>
2017-10-19 17:00:06 +0200

    distribution v0.0.5

---
+++ Statuses.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Statuses;
+(function (Statuses) {
+    Statuses[Statuses["pending"] = 0] = "pending";
+    Statuses[Statuses["success"] = 1] = "success";
+    Statuses[Statuses["failure"] = 2] = "failure";
+    Statuses[Statuses["error"] = 3] = "error";
+})(Statuses = exports.Statuses || (exports.Statuses = {}));
